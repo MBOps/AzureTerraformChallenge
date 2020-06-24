0d68a522fd1182fa58755887d5631de87c7e832a
Matt Boyd <mjb141@gmail.com>
2020-06-24 15:37:36 +0100

    Trying again

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "azurerm_app_service_plan" "asp" {
 
 # Provision the Azure App Service to host the main web site
 resource "azurerm_app_service" "webapp" {
-    name                = "${var.resource_prefix}-webapp"
+    name                = "${var.resource_prefix}-WebApp"
     location            = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
     app_service_plan_id = azurerm_app_service_plan.asp.id
