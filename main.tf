352fd9fa934ee7a8e2474c38a334bd02cead4896
Matt Boyd <mjb141@gmail.com>
2020-06-25 15:40:19 +0100

    Add ACR to terraform script

--- main.tf
+++ main.tf
@@ -132,6 +132,13 @@ resource "azurerm_container_group" "ACI" {
 
 }
 
+resource "azurerm_container_registry" "ACR" {
+  name                = "${var.resource_prefix}-ACR"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+}
+
 resource "azurerm_kubernetes_cluster" "AKS" {
   name                = "${var.resource_prefix}-AKS"
   location            = azurerm_resource_group.rg.location
