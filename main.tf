e3b8b2f04c549a51571146f23c51ee60fd67e208
Matt Boyd <mjb141@gmail.com>
2020-06-25 15:50:07 +0100

    remove - from ACR name

--- main.tf
+++ main.tf
@@ -133,7 +133,7 @@ resource "azurerm_container_group" "ACI" {
 }
 
 resource "azurerm_container_registry" "ACR" {
-  name                = "${var.resource_prefix}-ACR"
+  name                = replace("${var.resource_prefix}-ACR", "-", "")
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
