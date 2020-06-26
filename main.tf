c115ddab2ebf61b73472789128ece2afde03a64c
Matt Boyd <mjb141@gmail.com>
2020-06-26 17:41:57 +0100

    Adding StaticIP, secret and values yaml

--- main.tf
+++ main.tf
@@ -187,22 +187,27 @@ resource "azurerm_kubernetes_cluster" "AKS" {
   }
 }
 
+# static IP
+
+resource "azurerm_public_ip" "challenge5" {
+  name                = "${var.resource_prefix}-staticIP"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+}
+
+
+# kubernetes secret with consul-federation.yaml
+# helm > kubeconfig ?? 
+
+
 resource "helm_release" "consul" {
   name       = "azure-eats-aks-consul"
   repository = "https://helm.releases.hashicorp.com" 
   chart      = "consul"
 
-  # values = [
-  #   "${file("values.yaml")}"
-  # ]
+  values = [
+    "${file("values.yaml")}"
+  ]
 
-  set {
-    name  = "global.name"
-    value = "consul"
-  }
-
-  set {
-    name  = "global.datacenter"
-    value = "MBOps"
-  }
 }
