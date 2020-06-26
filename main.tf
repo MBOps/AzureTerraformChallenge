1a9de61e6d0e47cfe9bbcc771c5715d01df60dc8
Matt Boyd <boydy_6@hotmail.com>
2020-06-26 22:27:07 +0100

    updating aks node size

--- main.tf
+++ main.tf
@@ -173,7 +173,7 @@ resource "azurerm_kubernetes_cluster" "AKS" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_D1_v2"
+    vm_size    = "Standard_D2_v2"
   }
 
   addon_profile {
